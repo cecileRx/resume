a1ec3b0477f7511a8025d1ee613423d16f17015a
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-01-26 16:45:12 +0100

    elements removed from header

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -10,9 +10,7 @@ export default class Contactus extends Component {
             <h1><span>Get In Touch.</span></h1>
           </div>
           <div className="ten columns">
-            <p className="lead">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-              voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+            <p className="lead">Let me know if you have a proposition for the dream job, any kind of suggestion, idea, or just to say hello!
             </p>
           </div>
         </div>
@@ -56,33 +54,11 @@ export default class Contactus extends Component {
             <div className="widget widget_contact">
               <h4>Address and Phone</h4>
               <p className="address">
-                Jonathan Doe<br />
-                1600 Amphitheatre Parkway <br />
-                Mountain View, CA 94043 US<br />
-                <span>(123) 456-7890</span>
+                Cécile Rougnaux<br />
+                30 rue des 3 bornes <br />
+                75011 Paris<br />
+                <span>(0033)623 842 387</span>
               </p>
-            </div>
-            <div className="widget widget_tweets">
-              <h4 className="widget-title">Latest Tweets</h4>
-              <ul id="twitter">
-                <li>
-                  <span>
-                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                    Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                    <a href="#">http://t.co/CGIrdxIlI3</a>
-                  </span>
-                  <b><a href="#">2 Days Ago</a></b>
-                </li>
-                <li>
-                  <span>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                    eaque ipsa quae ab illo inventore veritatis et quasi.
-
-                    <a href= "#" > http://t.co/CGIrdxIlI3</a>
-                  </span>
-                  <b><a href="#">3 Days Ago</a></b>
-                </li>
-              </ul>
             </div>
           </aside>
         </div>
